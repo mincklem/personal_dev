f63f74d443a2de539e687381f5b522f34c72dc54
Max Minckler <mincklem@gmail.com>
2015-06-26 14:51:47 -0400

    working

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	  <link rel="stylesheet" type="text/css" href="css/main.css">
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+   <script type="text/javascript" src="js/touch-punch.js"></script>
    <script type="text/javascript" src="js/main.js"></script>
   <script type="text/javascript" src="js/html2canvas.js"></script>
 <script src="js/jquery-cookie-master/src/jquery.cookie.js"></script>
@@ -14,6 +15,7 @@
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
 <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+
 
 <!-- SHARE THIS -->
 
